65d6671c5472b523fd7eb07451b41d4ade7192f8
voquangphu1997 <110729819+voquangphu1997@users.noreply.github.com>
2022-08-23 09:03:47 +0700

    ngay

---
+++ display date.py
@@ -0,0 +1,17 @@
+date = int(input("Nhap so ngay: "))
+if date == 1:
+    print("Monday")
+elif date == 2:
+    print("Tuesday")
+elif date == 3:
+    print("Wednesday")
+elif date == 4:
+    print("Thursday")
+elif date == 5:
+    print("Friday")
+elif date == 6:
+    print("Saturday")
+elif date == 7:
+    print("Sunday")
+else:
+    print("error, out of range")
