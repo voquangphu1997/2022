3c81517180988184ad7406abd775b765bf4106c1
voquangphu1997 <110729819+voquangphu1997@users.noreply.github.com>
2022-09-25 16:49:44 +0700

    Dictionanry
    Bài ứng dụng quản lý sản phẩm_dùng dict

---
+++ dictionary_ungdungquanlysanpham.py
@@ -0,0 +1,44 @@
+def print_menu():
+    print("""
+    Enter your selection: 
+    1. View production list
+    2. Add a new production to the list
+    3. Edit the name of the production in list
+    4. Remove a production from the list
+ """)
+list = {
+        'a001': "book",
+        'a002': "cup",
+        'a003': "laptop",
+        'a004': "table",
+        'a005': "chair",
+    }
+def view_product_list():
+    print(list)
+
+def add_new_product_to_list():
+    product_id = input("Enter the id of product: ")
+    product_name = input("Enter the name of product: ")
+    if product_id in list:
+        print("Product ID is avaiable")
+        print("You can choose the 3rd option to edit the product name")
+    else:
+        list[product_id] = product_name
+        print(list)
+def edit_name_product_to_list():
+    product_id = input("Enter the id of product: ")
+    product_name = input("Enter the name of product: ")
+    list[product_id] = product_name
+    print(list)
+while True:
+    print_menu()
+    user_input = int(input("choose your option: "))
+    if user_input == 1:
+        view_product_list()
+    elif user_input == 2:
+        add_new_product_to_list()
+    elif user_input == 3:
+        edit_name_product_to_list()
+    elif user_input == 4:
+        break
+    
