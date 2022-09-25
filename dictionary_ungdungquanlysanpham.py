48c170e26ceea42550a636cf029e0ddb88b88813
voquangphu1997 <110729819+voquangphu1997@users.noreply.github.com>
2022-09-25 17:36:36 +0700

    Update dictionary_ungdungquanlysanpham.py

--- dictionary_ungdungquanlysanpham.py
+++ dictionary_ungdungquanlysanpham.py
@@ -5,6 +5,7 @@ def print_menu():
     2. Add a new production to the list
     3. Edit the name of the production in list
     4. Remove a production from the list
+    5. Exit
  """)
 list = {
         'a001': "book",
@@ -30,6 +31,14 @@ def edit_name_product_to_list():
     product_name = input("Enter the name of product: ")
     list[product_id] = product_name
     print(list)
+def remove_product_from_list():
+    product_id = input("Enter the id of product: ")
+    if product_id in list:
+        list.pop(product_id)
+        print(list)
+    else:
+        print("Product ID is unavaiable to remove")
+        print("You can choose the 3rd option to edit the product name")
 while True:
     print_menu()
     user_input = int(input("choose your option: "))
@@ -40,5 +49,7 @@ while True:
     elif user_input == 3:
         edit_name_product_to_list()
     elif user_input == 4:
+        remove_product_from_list()
+    elif user_input == 5:
         break
     
